a96c57cb020015c32fc01782989b94bda537b90d
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-25 17:28:37 +0530

    1

--- src/main/java/com/driver/SportsEventRegistration.java
+++ src/main/java/com/driver/SportsEventRegistration.java
@@ -14,22 +14,35 @@ public class SportsEventRegistration {
     public SportsEventRegistration() {
         registeredParticipants = new HashMap<>();
     }
-    
+
     public Map<String, String> getRegisteredParticipants() {
-    	//your code goes here
         return registeredParticipants;
     }
-    
+
     public void registerParticipant(String eventName, String participantName) {
-    	//your code goes here
+        LOGGER.log(Level.INFO, participantName + " registered for the event " + eventName);
+        registeredParticipants.put(eventName, participantName);
     }
 
     public void displayParticipants() {
-    	//your code goes here
+        if (registeredParticipants.isEmpty()) {
+            LOGGER.log(Level.WARNING, "No participants registered for any event.");
+        } else {
+            LOGGER.log(Level.INFO, "Registered Participants:");
+            for (Map.Entry<String, String> entry : registeredParticipants.entrySet()) {
+                LOGGER.log(Level.INFO, entry.getKey() + ": " + entry.getValue());
+            }
+        }
     }
 
     public void cancelRegistration(String eventName, String participantName) {
-    	//your code goes here
+        if (registeredParticipants.containsKey(eventName) &&
+                registeredParticipants.get(eventName).equals(participantName)) {
+            registeredParticipants.remove(eventName);
+            LOGGER.log(Level.WARNING, participantName + " canceled registration for the event " + eventName);
+        } else {
+            LOGGER.log(Level.SEVERE, "Error: No registration found for " + participantName + " in the event " + eventName);
+        }
     }
 
     public static void main(String[] args) {
@@ -47,4 +60,3 @@ public class SportsEventRegistration {
         registrationSystem.displayParticipants();
     }
 }
-
